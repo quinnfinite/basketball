927abb4197477213924e4e48c6408b3c43ce0fac
Quinn Royston <quinnroyston@Valhalla.lan>
2024-11-23 19:43:13 -0800

    Update type naming

--- api/src/lib/ballDontLie/index.ts
+++ api/src/lib/ballDontLie/index.ts
@@ -1,6 +1,6 @@
 const BASE_URL = "https://api.balldontlie.io/v1/"
 
-async function ballDontLie<T> (apiKey:string , endpoint: string): Promise<T> {
+async function ballDontLie<JsonData> (apiKey:string , endpoint: string): Promise<JsonData> {
     const url = `${BASE_URL}${endpoint}`;
 
     const response = await fetch(url, {
@@ -13,7 +13,7 @@ async function ballDontLie<T> (apiKey:string , endpoint: string): Promise<T> {
       throw new Error(`Request to Ball Don't Lie Failed. Response status: ${response.status}`);
     }
 
-    const json = await response.json<{ data: T }>();
+    const json = await response.json<{ data: JsonData }>();
     
     return json.data;
 } 
