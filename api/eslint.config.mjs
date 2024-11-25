e45c04a06f64567940566e29c382a249a371815f
Quinn Royston <quinnroyston@Valhalla.lan>
2024-11-24 22:47:50 -0800

    Modify linting

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {ignores: ["*.wrangler"]},
+  {ignores: ["*.wrangler", "node_modules/", "**/public"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
